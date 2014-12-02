bf1917c01d14654be6a2335185b6f161cdfafaf3
voloda <vladimir.kloz@gmail.com>
2014-12-02 21:09:22 +0100

    Issue #12: Create a project using Microsoft Unity as IoC container

---
+++ 010_IoCWithoutUnity/Operations.cs
@@ -0,0 +1,15 @@
+﻿namespace IoCWithoutUnity
+{
+    public interface IOperation
+    {
+        int Evaluate(int a, int b);
+    }
+
+    public class Plus : IOperation
+    {
+        public int Evaluate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
