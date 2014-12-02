8f4aaff51d96ad8342835262c416565fee1f4e8b
voloda <vladimir.kloz@gmail.com>
2014-12-02 22:42:47 +0100

    Issue #1: Create project to demonstrate life time management

---
+++ 040_UnityLifetimeManagement/OperationFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Practices.Unity;
+
+namespace UnityLifetimeManagement
+{
+    public interface IOperationFactory
+    {
+        IOperation Create(string token);
+    }
+
+    public class OperationFactory : IOperationFactory
+    {
+        private readonly IUnityContainer m_Container;
+
+        public OperationFactory(IUnityContainer container)
+        {
+            m_Container = container;
+        }
+
+        public IOperation Create(string token)
+        {
+            switch (token)
+            {
+                case "+":
+                    return m_Container.Resolve<IOperationPlus>();
+                case "-":
+                    // return m_Container.Resolve<IOperationMinus>();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
