c4c806adf1a664fcb9b97a305c542526d54c25ad
voloda <vladimir.kloz@gmail.com>
2015-01-18 22:20:28 +0100

    Issue #9: Create a project how to deal with parameterized constructor

---
+++ 110_UnityResolutionOverrides/ConsoleResultWriter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace UnityResolutionOverrides
+{
+    public interface IConsoleResultWriter
+    {
+        void WriteResult(int value);
+    }
+
+    public interface IConsoleAndFileResultWriter
+    {
+        void WriteResult(int value);
+    }
+
+    /// <summary>
+    /// </summary>
+    public class ConsoleResultWriter : IConsoleResultWriter
+    {
+        public void WriteResult(int value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    public class ConsoleAndFileResultWriter : IConsoleAndFileResultWriter, IDisposable
+    {
+        private readonly IConsoleResultWriter _consoleResultWriter;
+        private readonly StreamWriter _output;
+
+        public ConsoleAndFileResultWriter(string fileName, IConsoleResultWriter consoleResultWriter)
+        {
+            _consoleResultWriter = consoleResultWriter;
+            _output = File.CreateText(fileName);
+        }
+
+        public void WriteResult(int value)
+        {
+            _consoleResultWriter.WriteResult(value);
+            _output.WriteLine(value);
+        }
+
+        public void Dispose()
+        {
+            if (_output != null)
+            {
+                _output.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+    }
+
+}
